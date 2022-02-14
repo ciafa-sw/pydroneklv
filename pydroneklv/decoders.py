20120ed17aa53dfa830a4eb1635a09dc85de7691
Diogo Silva <dasilva@academiafa.edu.pt>
2022-02-14 13:11:30 +0000

    minor PEP8 errors, minor refactoring of timestamp decoder, fixed types

--- pydroneklv/decoders.py
+++ pydroneklv/decoders.py
@@ -1,10 +1,18 @@
 import datetime
 import struct
 
+from typing import Tuple
+
+from .utils import *
+
 
 def raiseLenError(payload, correct_length):
     if len(payload) != correct_length:
         raise Exception(f'size of payload should be {correct_length} bytes, but got {len(payload)}')
+
+
+def decode_timestamp_seconds(buf: bytes) -> int:
+    return struct.unpack(">Q", buf)[0]
 
 
 def decodeTimeStamp(buf):
@@ -17,8 +25,8 @@ def decodeTimeStamp(buf):
                     var unix_timestamp = bigInt(buffer.join(''), 16).toString();
                     return new Date(unix_timestamp / 1000); // Convert from microseconds to milliseconds, and return
     """
-    uInt64 = struct.unpack(">Q", buf)[0]
-    dt = datetime.datetime.fromtimestamp(uInt64 / 1000000)  # convert µs to seconds
+    uint64 = decode_timestamp_seconds(buf)
+    dt = datetime.datetime.fromtimestamp(uint64 / 1e6)  # convert µs to seconds (*1e-6)
     return dt
 
 
@@ -53,11 +61,13 @@ def decodeLatitude(buf):
     int32 = struct.unpack(">i", buf)[0]
     return 180 / 0xFFFFFFFE * int32
 
+
 def decodeLongitude(buf: bytes) -> float:
     # tag 14, defined in page 53
     raiseLenError(buf, 4)
     int32 = struct.unpack(">i", buf)[0]
     return 360 / 0xFFFFFFFE * int32
+
 
 def decodeAltitude(buf: bytes) -> float:
     # tag 15, defined in page 54, example provided there is wrong
@@ -85,6 +95,7 @@ def decodeSensorRelElevationAngle(buf):
     uInt32 = struct.unpack(">i", buf)[0]
     return 360 / 0xFFFFFFFE * uInt32
 
+
 def decodeSlantRange(buf):
     uInt32 = struct.unpack(">I", buf)[0]
     return 5000000 / 0xFFFFFFFF * uInt32
@@ -110,20 +121,39 @@ def checksum(packet):
     return cs == (packet[-2] << 8 + packet[-1])
 
 
-def computeCrc(buf: bytes) -> int:
-    crc = 0
-    for i, b in enumerate(buf):
-        crc += b << (8 * ((i + 1) % 2))
-    return crc & 0xffff
-
-
 def verifyCrc_old(p):
     packetCrc = (p[-2] << 8) + p[-1]
     computedCrc = computeCrc(p[:-2])
     return packetCrc == computedCrc
 
 
-def verifyCrc(p: bytes, startIndex: int, size: int) -> bool:
+def verify_crc(p: bytes, startIndex: int, size: int) -> bool:
     packetCrc = (p[size - 2] << 8) + p[size - 1]
     computedCrc = computeCrc(p[startIndex:size - 2])
     return packetCrc == computedCrc
+
+
+def decode_passthrough(buf: bytes) -> bytes:
+    return buf
+
+
+def decode_length(buf: bytes, idx: int = 0) -> Tuple[int, int]:
+    """decode the length of the payload of a given packet
+    returns a tuple (number of bytes encoding length, number of bytes of payload)
+
+    """
+    is_longform = buf[idx] & 0b10000000  # first bit specifies if long form or not
+
+    if not is_longform:
+        klv_size = buf[idx] & 0b01111111  # 7 last bits specify the length of the payload
+        return 1, klv_size
+    else:
+        n_size_bytes = buf[idx] & 0b01111111  # 7 last bits specify the number of bytes encoding length
+        klv_size = 0
+        i = 1
+        # TODO check if there are enough bytes
+        while i <= n_size_bytes:
+            klv_size = klv_size << 8
+            klv_size += buf[idx+i]
+            i += 1
+        return 1 + n_size_bytes, klv_size  # the "1+" is for the first byte specifying how many length bytes exist
