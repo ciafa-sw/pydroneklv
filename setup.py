19b2699ec705d7d669d40f3a2bb757f4bccc297e
Diogo Silva <dasilva@academiafa.edu.pt>
2022-02-15 09:47:10 +0000

    bumped version from last commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pydroneklv',
-    version='0.1.0',
+    version='0.1.1',
     description='Package to decode KLV packets from TS stream according to standard ST 0601.8',
     url='https://bitbucket.org/uavdevs/pydroneklv',
     author='Diogo Silva',
@@ -14,7 +14,6 @@ setup(
 
     classifiers=[
         'Development Status :: 1 - Early development',
-        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
     ],
 )
